aabc6ce9a7e6883490c496cf76fb27e6f7329038
OozyGrub <oozygrub@hotmail.com>
2020-10-15 10:30:03 +0700

    create subject type

--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,14 @@ export interface Faculty {
 export interface FacultyMap {
   [code: string]: Omit<Faculty, "code">;
 }
+
+export interface Subject {
+  code: string;
+  codeName: string;
+  facultyCode: string;
+  name_en: string;
+  name_th: string;
+  isClosed: boolean;
+  openSemester: string;
+  closeSemester: string;
+}
